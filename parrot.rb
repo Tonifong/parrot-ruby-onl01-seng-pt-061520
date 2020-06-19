6f597d5c6805a43575cea2f588cfb0e9453665a5
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-19 03:01:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
-  puts "Squawk!"
-  return "Squawk!"
+def parrot(phrase="Squawk!")
+  puts phrase
+  return phrase
   end
-def parrot("Squawk")
-  puts 
